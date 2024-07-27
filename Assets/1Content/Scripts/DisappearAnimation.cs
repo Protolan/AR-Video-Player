452f480437e426d589c6e5d7fbcd69f9586050c1
Rustam Mirzakhmedov <rst@instafilter.ru>
2024-07-27 22:19:57 +0200

    Trackable Layers Fix

--- Assets/1Content/Scripts/DisappearAnimation.cs
+++ Assets/1Content/Scripts/DisappearAnimation.cs
@@ -1,12 +1,10 @@
 using DG.Tweening;
 using UnityEngine;
-using UnityEngine.Events;
 
 namespace _1Content.Scripts
 {
     public class DisappearAnimation: MonoBehaviour
     {
-        public UnityEvent onCompleted;
         public Ease easing = Ease.Linear;
         public float duration = 0.5f;
 
@@ -17,7 +15,6 @@ namespace _1Content.Scripts
             transform.DOScale(_endScale, duration).OnComplete(() =>
             {
                 gameObject.SetActive(false);
-                onCompleted.Invoke();
             });
         }
     }
